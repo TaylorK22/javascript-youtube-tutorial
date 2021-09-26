2a1edad0abaeb95e17ffb21c0874c14070e58d30
Taylork22 <tjwkoenen@gmail.com>
2021-09-25 20:13:02 -0400

    learning/implementing arrays/loops into game

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,17 +1,45 @@
-let firstCard = 11;
-let secondCard = 2;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.querySelector("#cards-el");
+
+let player = {
+  Name: "Taylor",
+  Chips: 145 
+}
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.Name + ": $" + player.Chips;
+
+function getrandomcard() {
+  let drawnCard = Math.floor(Math.random() * 13) + 1;
+  if (drawnCard === 1) {
+    return 11;
+  } else if (drawnCard > 10) {
+    return 10;
+  } else {
+    return drawnCard;
+  }
+}
 function startgame() {
+  isAlive = true;
+  let firstCard = getrandomcard();
+  let secondCard = getrandomcard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+
   rendergame();
 }
 function rendergame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card? ";
   } else if (sum === 21) {
@@ -22,15 +50,14 @@ function rendergame() {
     isAlive = false;
   }
   messageEl.textContent = message;
-  sumEl.textContent = "Sum: " + sum;
-  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
 }
 
 function newcard() {
-  console.log("draw a new card");
-  let card = 7;
-  cards.push(card)
-  sum += card;
-  rendergame();
-  console.log(cards)
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getrandomcard();
+    cards.push(card);
+    sum += card;
+    rendergame();
+    console.log(cards);
+  }
 }
